26cc92c37c3887a3cab5c857a8ff25c82bb3a88f
Nicholas Keshavarz-Nia <nickmknia@gmail.com>
2013-09-17 21:09:50 -0400

    Began the backbone of the index(homepage)

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<!DOCTYPE html>
 <?php
-
-echo "Capstone Connect";
-
-?>
+	echo "<html>
+				<head>SC Capstone Connect
+				<title>SC Capstone Connect</title>
+				</head>
+				<br />
+				<br />
+				<body>This is the body of our site</body>
+		  </html>
+	";
+	
+	
+	?>
