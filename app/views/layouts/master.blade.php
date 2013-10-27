ec28e46a9c98bf9ddb28a3b7bcac0be6131b662a
Skylar Smith <skysmith112@gmail.com>
2013-10-26 23:33:34 -0400

    Installed twitter bootstrap and started master template

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            @section('title')
+            Capstoneconnect
+            @show
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+        <!-- CSS are placed here -->
+        {{ HTML::style('css/bootstrap.css') }}
+        <style>
+        @section('styles')
+            body {
+                padding-top: 60px;
+            }
+        @show
+        </style>
+        {{ HTML::style('css/bootstrap-responsive.css') }}
+ 
+    </head>
+ 
+    <body>
+    
+    </body>
+</html>
