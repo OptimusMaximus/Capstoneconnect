b82b0ae8cce9462d26bf4b08b5bab9f179523628
Nick Keshavarz-Nia <nickmknia@gmail.com>
2013-11-05 14:20:43 -0500

    Edited the questionnaire page.

--- app/views/questionnaire.blade.php
+++ app/views/questionnaire.blade.php
@@ -5,16 +5,16 @@
 @stop
 
 @section('styles')
-.bigWhite {
-	background-color: #FFFFFF;
-	height: 700px;
-	width: 1500px;
-	position: relative;
+.BigWhite {
+	background-color:#FFFFFF;
+	height:600px;
+	width:900px;
 	margin: 0 auto;
-	}
+
+}
 
 .insideBigWhite {
-	background-color: #FFFFFF;
+	background-color: #FCFCFC;
 	color: #000000;
 	height: 650px;
 	width: 1400px;
@@ -34,11 +34,21 @@
 	top: 25px; {{-- 25px from top of insideBigWhite --}}
 }
 
+.radio-inline {
+	width:900px;
+
+}
+
+.form-control {
+	margin-left: 50px;
+	width: 600px;
+}
+
 .linkNextQ {
 	background-color: #FFFFFF;
 	height: 50px;
 	width: 200px;
-	position: absolute;
+	 
 	left: 50px; {{-- 50px from left of questions --}}
 	top: 600px; {{-- 600px from top of questions --}}
 	
@@ -57,17 +67,156 @@
 @stop
 
 @section('content')
-    <div class = bigWhite>
-    	<div class = insideBigWhite> 
+    <div class = BigWhite>
+    	 
     		<h1>Group Member Questionnaire</h1>
     		<div class = questions> 
-    			<p>Questions go here.</p>	
-    			<p>More questions.</p>
-    		</div>
+    			<p>Question 1</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+    			<p>Question 2</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+				<p>Question 3</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+				<p>Question 4</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+				<p>Question 5</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+				<p>Question 6</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+				<p>Question 7</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+				<p>Question 8</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+				<p>Question 9</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
+				<p>Question 10</p>	
+    			<label class="radio-inline">
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 1
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 2
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 3
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 4
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 5
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 6
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 7
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 8
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 9
+ 			    <input type="radio" id="inlineRadio1" value="option1"> 10
+				</label>
+				<textarea class="form-control" rows="3" placeholder="Comments go here"></textarea>
     		<div class = linkNextQ>
     			<a href="#NextQuestionnaire" class = "blue" target="_blank">Proceed to next evaluation</a>
     		</div>
     	</div>
+    		
+    	
     </div>
     
 @stop
