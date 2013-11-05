922ffd2d7830a4761addc7a668cb635d196a95e5
frankets <frankets@email.sc.edu>
2013-11-04 20:23:44 -0500

    Pushed homepage version 1.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -11,25 +11,40 @@
 	margin: 0 auto;
 
 }
-.Announcements {
-	background-color:Blue;
+.Announcements {	
+	background-color:#73000A;
 	height:550px;
 	width:300px;
 	margin: 300 auto;
 	margin:30px 30px 30px 30px;
 	border:2px solid black;
 	float:left;
-	
+	} 	
+
+	.InsideAnnouncement {
+	background-color:Grey;
+	height:500px;
+	width:275px;
+	border:2px scrollingd black;
+	margin: 0 auto;
 	
 }
 .RecentActivity {
-	background-color:Green;
+	background-color:#73000A;
 	height:200px;
 	width:490px;
 	margin:30px 30px 10px 10px;
 	float:right;
 	border:2px solid black;
-	
+	}
+
+	.InsideRecentActivity {
+	background-color:grey;
+	height:165px;
+	width:475px;
+	margin-right:5px;
+	float:right;
+	border:2px solid black;
 	
 }
 	.Calendar {
@@ -47,17 +62,29 @@
 
 @section('content')
 
+
 <div class="BigWhite">
  <div class="Announcements">
+
  <h>
  Announcements
  </h>
-
+<div class="InsideAnnouncement">
+  <h>
+  Here
+  </h>
+  </div>
  </div>
+  
 <div class="RecentActivity">
 <h>
 Recent Activity
 </h>
+<div class="InsideRecentActivity">
+<h>
+Activity here!
+</h>
+</div>
  </div>
  <div class="Calendar">
 <iframe src="https://www.google.com/calendar/embed?src=seanfrankett%40gmail.com&ctz=America/New_York" style="border: 20" width="490" height="320" frameborder="0" scrolling="no"></iframe>
