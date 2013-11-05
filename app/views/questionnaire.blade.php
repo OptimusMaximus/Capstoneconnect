8981619bcbb53cfaabfcc8f6185873c4fc581d61
OptimusMaximus <maximusbrandel@gmail.com>
2013-11-05 12:25:15 -0500

    Questionnaire updated

--- app/views/questionnaire.blade.php
+++ app/views/questionnaire.blade.php
@@ -7,21 +7,48 @@
 @section('styles')
 .bigWhite {
 	background-color: #FFFFFF;
-	{{--height: 600px;
-	width: 900px;--}}
-	height: 50%;
-	width: 60%;
+	height: 700px;
+	width: 1500px;
+	position: relative;
 	margin: 0 auto;
 	}
 
 .insideBigWhite {
-	background-color: #AAAAAA;
-	{{-- height: 500px;
-	width: 800px; --}}
-	height: 80%;
-	width 50%;
-	margin: 0px 20px 0px 20px;
+	background-color: #FFFFFF;
+	color: #000000;
+	height: 650px;
+	width: 1400px;
+	position: absolute;
+	left: 50px; {{--30px from left of bigWhite --}}
+	top: 25px; {{-- 5px from top of bigWhite --}}
 	}
+
+.questions{
+	background-color: #FFFFFF;
+	color: #000000;
+	text-align: left;
+	height: 500;
+	width: 1200
+	postion: absolute;
+	left: 20px; {{-- 20px from left of insideBigWhite --}}
+	top: 25px; {{-- 25px from top of insideBigWhite --}}
+}
+
+.linkNextQ {
+	background-color: #FFFFFF;
+	height: 50px;
+	width: 200px;
+	position: absolute;
+	left: 50px; {{-- 50px from left of questions --}}
+	top: 600px; {{-- 600px from top of questions --}}
+	
+}
+
+	a.blue:link {color:#0000FF}      /* unvisited link color blue */
+    a.blue:visited {color:#0000FF}  /* visited link color blue*/
+    a.blue:hover {color:rgb(95,87,79);}  /* mouse over link color Pluff mud*/
+    a.blue:focus {color:rgb(95,87,79);}  /* link of focus color Pluff mud*/
+    a.blue:active {color:rgb(178,180,179);}  /* selected link color State House Gray*/
 
 @stop
 
@@ -31,7 +58,16 @@
 
 @section('content')
     <div class = bigWhite>
-    	<p> Hello!</p>
+    	<div class = insideBigWhite> 
+    		<h1>Group Member Questionnaire</h1>
+    		<div class = questions> 
+    			<p>Questions go here.</p>	
+    			<p>More questions.</p>
+    		</div>
+    		<div class = linkNextQ>
+    			<a href="#NextQuestionnaire" class = "blue" target="_blank">Proceed to next evaluation</a>
+    		</div>
+    	</div>
     </div>
     
 @stop
