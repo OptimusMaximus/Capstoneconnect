5492c52c2ea619245759a328fe9368c50db902fe
Skylar Smith <skysmith112@gmail.com>
2013-11-05 22:17:19 -0500

    admin tools

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,9 +12,8 @@
         <link rel="stylesheet" href="css/bootstrap.css" />
         <link rel="stylesheet" href="css/bootstrap-responsive.css" />
         <style>
-        @yield('styles')
             body {
-                padding-top: 30px;
+                padding-top: 60px;
                 background-color: #73000A;
                 color: #FFFFFF;
                 text-align: center;
@@ -37,20 +36,30 @@
             .nav-pills{
                 display: inline-block;
             }
+            ul.nav-pills a:link {background-color: none}
+            ul.nav-pills a:hover {background-color:rgb(178,180,179);}
+            ul.nav-pills a:focus {background-color:rgb(178,180,179);}
+            ul.nav-pills a:active {background-color:rgb(178,180,179);}
+
             a:link {color:#FFFFFF;}      /* unvisited link white */
             a:visited {color:#FFFFFF}  /* visited link white*/
-            a:hover {color:rgb(95,87,79);}  /* mouse over link State House Gray*/
-            a:focus {color:rgb(95,87,79);}
-            a:active {color:rgb(178,180,179);}  /* selected link Pluff Mud*/ 
+            a:hover {color:#000000;}  /* mouse over link State House Gray*/
+            a:focus {color:#000000;}
+            a:active {color:#000000;}  /* selected link Pluff Mud*/ 
+            @yield('styles')
         </style>
     </head>
     <div class="text-center">
         @yield('header')
         <ul class="nav nav-pills">
             <li><a href="/public">Home</a></li>
-            <li><a href="/public/questionnaire">Questionaire</a></li>
+            <li><a href="#Questionaire">Questionaire</a></li>
             <li><a href="/public/mygrades">My Grades</a></li>
+
             <li><a href="/public/help">Help</a></li>
+
+            <li><a href="#Help">Help</a></li>
+
             <li><a href="/public/login">Logout</a></li>
         </ul>
     </div>
