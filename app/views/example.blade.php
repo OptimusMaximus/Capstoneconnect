aab2a870c5d8f93e3d1c08a873e5800aaffe9f89
nickmknia <nickmknia@gmail.com>
2013-11-05 19:07:35 -0500

    Delete example.blade.php

--- app/views/example.blade.php
+++
@@ -1,9 +0,0 @@
-<h1> Dear Lord Otwell</h1>
-<p>blah blah</p>
-
-{{-- Blade way to format php --}}
-<p>{{ date('m/d/y') }}</p>
-
-{{-- Three brackets cause output to be escaped --}}
-<p>{{{ '<script>alert("CHUNKY BACON!");</script>' }}}</p>
-
