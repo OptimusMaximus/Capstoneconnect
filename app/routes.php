ec85d9d18f2b7f007bc614907637c8c213862642
OptimusMaximus <maximusbrandel@gmail.com>
2013-11-15 23:20:45 -0500

    Updated routes so you can't access pages until you login

--- app/routes.php
+++ app/routes.php
@@ -10,12 +10,25 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+//Route::get('/', array('uses' => 'AuthController@getLogin'));
+Route::get('/login', array('uses' => 'AuthController@getLogin'));
+Route::post('/login', array('uses' => 'AuthController@postLogin'));
+Route::get('/logout', array('uses' => 'AuthController@getLogout'));
 
-Route::get('/', 'HomeController@showWelcome');
-Route::get('/help', 'HelpController@showWelcome');
-Route::get('/admin', 'AdminToolsController@makePage');
-Route::get('/questionnaire', 'QuestionnaireController@showWelcome');
-Route::get('/mygrades', 'GradesController@showWelcome');
-Route::get('/login', 'LoginController@showWelcome');
+Route::group(array('prefix' => '', 'before' => 'auth'), function()
+{
+        Route::get('/home', 'HomeController@showWelcome');
+		Route::get('/help', 'HelpController@showWelcome');
+		Route::get('/admin', 'AdminToolsController@makePage');
+		Route::get('/questionnaire', 'QuestionnaireController@showWelcome');
+		Route::get('/mygrades', 'GradesController@showWelcome');
+});
 
 
+//For testing
+Route::get('/test', function()
+{
+    $users = User::all();
+
+    return View::make('/test')->with('users', $users);
+});
