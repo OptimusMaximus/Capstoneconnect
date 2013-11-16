05a335efea8b935b99ac012ee4b617f01784dad7
OptimusMaximus <maximusbrandel@gmail.com>
2013-11-15 23:06:14 -0500

    added Sentry alias and provider

--- app/config/app.php
+++ app/config/app.php
@@ -113,6 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Cartalyst\Sentry\SentryServiceProvider',
 
 	),
 
@@ -178,6 +179,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Sentry' 		  => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
 
 	),
 
