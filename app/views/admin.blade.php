db3ed745a2f96d7ad433a707494f7b6955ace169
Nick Keshavarz-Nia <nickmknia@gmail.com>
2013-12-05 21:24:59 -0500

    Added a table to the View Evaluations page

--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -163,20 +163,59 @@
           </div>
           @endforeach
         </div>
-        <?php $evaluations=Evaluation::all();?> 
+        <?php $mostRecentEvalDate = Evaluation::max('created_at');
+                $evaluation = Evaluation::where('created_at', $mostRecentEvalDate)->first();
+            ?> 
+        <?php $mostRecentAnswerDate = Answer::max('created_at');
+                $answer = Answer::where('created_at', $mostRecentAnswerDate)->first();
+            ?>     
         <table class="table table-bordered table-groups pull-right">
-          @foreach($evaluations as $evaluation)
+          
           <tr>
-            <td>fasd</td>
-            <td>adsf</td>
-            <td>test</td>
+            <td>{{$evaluation->q1}}</td>
+            <td>{{$answer->ans1}}</td>
           </tr>
           <tr>
-            <td>adsf</td>
-            <td>dsfa</td>
-            <td>dsfa</td>
+            <td>{{$evaluation->q2}}</td>
+            <td>{{$answer->ans2}}</td>
           </tr>
-          @endforeach
+          <tr>
+            <td>{{$evaluation->q3}}</td>
+            <td>{{$answer->ans3}}</td>
+          </tr>
+          <tr>
+            <td>{{$evaluation->q4}}</td>
+            <td>{{$answer->ans4}}</td>
+          </tr>
+          <tr>
+            <td>{{$evaluation->q5}}</td>
+            <td>{{$answer->ans5}}</td>
+          </tr>
+          <tr>
+            <td>{{$evaluation->q6}}</td>
+            <td>{{$answer->ans6}}</td>
+          </tr>
+          <tr>
+            <td>{{$evaluation->q7}}</td>
+            <td>{{$answer->ans7}}</td>
+          </tr>
+          <tr>
+            <td>{{$evaluation->q8}}</td>
+            <td>{{$answer->ans8}}</td>
+          </tr>
+          <tr>
+            <td>{{$evaluation->q9}}</td>
+            <td>{{$answer->ans9}}</td>
+          </tr>
+          <tr>
+            <td>{{$evaluation->q10}}</td>
+            <td>{{$answer->ans10}}</td>
+          </tr>
+          <tr>
+            <td>Comments</td>
+            <td>{{$answer->comment}}</td>
+          </tr>
+
         </table>
       </div>
     </div><!-- eval -->
