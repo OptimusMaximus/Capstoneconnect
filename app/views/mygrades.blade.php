d171336ca82a31b41f3e788781e59def14f546fc
OptimusMaximus <maximusbrandel@gmail.com>
2014-01-26 20:40:14 -0500

    Reformatted

--- app/views/mygrades.blade.php
+++ app/views/mygrades.blade.php
@@ -14,48 +14,32 @@
 
 @section('content')
 
-
 <div class="GradesWhite">
 	<div class="GradeAverageText">
- 	<h>
- 		Your current average is: 
- 	</h>
- </div>
- <div class="Evals">
-
-
-
-<div class="btn-group-vertical" style="margin-top:20px;">
-<button type="button" class="btn btn-default">Evaluations 1</button>
-
-  <button type="button" class="btn btn-default">Evaluations 2</button>
-  <button type="button" class="btn btn-default">Evaluations 3</button>
-  <button type="button" class="btn btn-default">Evaluations 4</button>
-  <button type="button" class="btn btn-default">Evaluations 5</button>
-  <button type="button" class="btn btn-default">Evaluations 6</button>
-  <button type="button" class="btn btn-default">Evaluations 7</button>
-  <button type="button" class="btn btn-default">Evaluations 8</button>
-
-
-
-
- </div>
+ 	  <h>Your current average is: </h>
+  </div>
+  <div class="Evals">
+    <div class="btn-group-vertical" style="margin-top:20px; margin-left:5px; margin-right:5px;">
+      <button type="button" class="btn btn-default">Evaluations 1</button>
+      <button type="button" class="btn btn-default">Evaluations 2</button>
+      <button type="button" class="btn btn-default">Evaluations 3</button>
+      <button type="button" class="btn btn-default">Evaluations 4</button>
+      <button type="button" class="btn btn-default">Evaluations 5</button>
+      <button type="button" class="btn btn-default">Evaluations 6</button>
+      <button type="button" class="btn btn-default">Evaluations 7</button>
+      <button type="button" class="btn btn-default">Evaluations 8</button>
+    </div>
+  </div>
   <div class="WeeklyGrades">
- </div>
- </div>
+    <p>Weekly grades go here</p>
+  </div> 
  
-<div class="AllGrades">
-<h>
-All grades go here!
-</h>
-<div class="InsideAllGrades">
-<h>
-More grade stuff.
-</h>
-</div>
- </div>
-
-
-  
+  <div class="AllGrades">
+    <div class="InsideAllGrades">
+      <h>All grades go here!</h>
+      <h>More grade stuff.</h>
+    </div>
+  </div>
+</div> 
 
 @stop
