80c9a8584f348d35918ebd515afed642b02b61ba
Skylar Smith <skysmith112@gmail.com>
2014-02-07 13:27:49 -0500

    rounded navbar

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -25,7 +25,7 @@
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -35,7 +35,7 @@
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="navbar-collapse">
           <ul class="nav navbar-nav">
             <li><a href="/Capstoneconnect/public/home">Home</a></li>
             <li><a href="/Capstoneconnect/public/questionnaire">Questionnaire</a></li>
