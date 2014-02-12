31f45c224cf88fae0d37c9c1e6ed2702899795c4
OptimusMaximus <maximusbrandel@gmail.com>
2014-02-11 19:46:08 -0500

    minor changes

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -11,6 +11,7 @@
         <!-- CSS are placed here -->
         {{ HTML::style('css/bootstrap.css') }}
         {{ HTML::style('css/base.css', array('type' => 'text/css')) }}
+        {{ HTML::style('css/master-bootstrap-overwrite.css') }}
         {{ HTML::style('css/small-device.css', array('type' => 'text/css','media'=> 'only screen and (max-width: 600px), only screen and (max-device-width: 600px)')) }}
         @yield('stylesheets')
         <style>
@@ -22,39 +23,18 @@
         <script src="js/bootstrap.js"></script>
         @yield('head')
     </head>
-
-    <nav class="navbar navbar-default" role="navigation">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Capstone Connect</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navbar-collapse">
-          <ul class="nav navbar-nav">
+    <div class="text-center">
+        <h1>@yield('header')</h1>
+        <ul class="nav nav-pills">
             <li><a href="/Capstoneconnect/public/home">Home</a></li>
             <li><a href="/Capstoneconnect/public/questionnaire">Questionnaire</a></li>
             <li><a href="/Capstoneconnect/public/mygrades">My Grades</a></li>
             <li><a href="/Capstoneconnect/public/help">Help</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
             <li><a href="/Capstoneconnect/public/logout">Logout</a></li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
+        </ul>
+    </div>
     <body>
-        <h1><u><b>@yield('header')</u></b></h1> 
-        <div class="BigWhite container">
-            @yield('content')
-        </div>
+        @yield('content')
            
     </body>
 </html>
