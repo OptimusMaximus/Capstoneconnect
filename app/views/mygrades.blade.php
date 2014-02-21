773993a5be6ab6382c0311385b09c640b941b406
Bensnorman <Bensnorman@gmail.com>
2014-02-20 23:17:41 -0500

    My Grades

--- app/views/mygrades.blade.php
+++ app/views/mygrades.blade.php
@@ -58,15 +58,13 @@ $a9 = DB::table('answers')->where('answered_about', '=', $user->id)->avg('ans9')
 $a10 = DB::table('answers')->where('answered_about', '=', $user->id)->avg('ans10');
 $avg = ($a1 + $a2 + $a3 + $a4 + $a5 + $a6 + $a7  + $a8  + $a9 + $a10)/10;
 ?>
-  
-@endforeach  
-
-
-                        <tr bgcolor="#73000A">
+   <tr bgcolor="#73000A">
                             <td><font color = 'White'>{{$user->first_name}}</td>
                             <td><font color = 'White'>{{$user->last_name}}</td>
                             <td><font color = 'White'>{{round($avg,2)}}/10 or {{round($avg*10,2)}}%</td>
                         </tr>
+@endforeach  
+
 
 
 
