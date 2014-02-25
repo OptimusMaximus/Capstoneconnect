ac882f6888219634516982f79847a9730a1c557d
OptimusMaximus <maximusbrandel@gmail.com>
2014-02-25 14:56:11 -0500

    Added Student ID

--- app/controllers/ExportCSVController.php
+++ app/controllers/ExportCSVController.php
@@ -16,7 +16,7 @@ class ExportCSVController extends BaseController {
 		$file = fopen('Downloads/ExportedData.csv', 'w'); 
 
 		//Header
-		fputcsv($file, array('Evaluation ID', 'First Name', 'Last Name', 'Answered By First Name', 'Answered By Last Name', 'Question 1', 'Question 2', 'Question 3', 
+		fputcsv($file, array('Evaluation ID', 'Student ID', 'First Name', 'Last Name', 'Answered By First Name', 'Answered By Last Name', 'Question 1', 'Question 2', 'Question 3', 
 			'Question 4', 'Question 5', 'Question 6', 'Question 7', 'Question 8', 'Question 9', 'Question 10'));
 		
 		//Get 'eid' variable from download_csv.blade view
@@ -26,7 +26,7 @@ class ExportCSVController extends BaseController {
 			if($row->eid == $eid) {
 				$user = User::find($row->answered_about);
 				$answeredBy = User::find($row->answered_by);
-				fputcsv($file, array($row->eid, $user->first_name, $user->last_name, $answeredBy->first_name, $answeredBy->last_name,
+				fputcsv($file, array($row->eid, $user->id, $user->first_name, $user->last_name, $answeredBy->first_name, $answeredBy->last_name,
 					$row->ans1, $row->ans2, $row->ans3, $row->ans4, $row->ans5, $row->ans6, $row->ans7, $row->ans8, $row->ans9, $row->ans10));
 			}
 		}
