56d11b1a88081d9c1890b111fec434259ea50a81
frankets <frankets@email.sc.edu>
2014-03-24 15:22:10 -0400

    Splash screen resize

--- app/views/splash.blade.php
+++ app/views/splash.blade.php
@@ -13,9 +13,13 @@
 @stop
 
 @section('content')
-
-<div>
 <center>
+<br><br>
+ <div class="col-xs-10 col-xs-offset-1 col-sm-9 col-sm-offset-2 col-md-8 col-md-offset-3 col-lg-6 col-lg-offset-3" style = "background-color: #73000A" style ="margin-left:325px" >
+ <div class="responsive-iframe-container iframe">
+
 <iframe width="640" height="400" src="//www.youtube.com/embed/vvc5I5_fsFs?autoplay=1" frameborder="0" allowfullscreen></iframe>
  </div> 
+ </center>
+ </div>
 @stop
