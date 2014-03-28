e0915c6764c10b82591cb634f43160607e266af4
frankets <frankets@email.sc.edu>
2014-03-27 23:00:39 -0400

    new eval page with code commented

--- app/views/evaluation_new.blade.php
+++ app/views/evaluation_new.blade.php
@@ -17,8 +17,48 @@ New Evaluation
 	{{ Form::open(        
 	     array('url' => URL::route('evaluation.store'),
 	                'role' => 'form'))}}
-	    @for($i = 1; $i <= 10; $i++)
+	   
+<!--Code to manupulate for add and remove button
+<script type="text/javascript">
+    $(document).ready(function(){
+        $counter = 0; // initialize 0 for limitting textboxes
+        $('#buttonadd').click(function(){
+            if ($counter < 10)
+            {
+                $counter++;
+                $('#buttondiv').append('<div><label>Textbox #'+$counter+'</label><input type="text" name="textbox[]" class="textbox" value="" /></div>');
+            }else{
+                alert('You cannot add more than 10 textboxes');
+            }
+        });
 
+        $('#buttonremove').click(function(){
+            if ($counter){
+                $counter--;
+                $('#buttondiv .textbox:last').parent().remove(); // get the last textbox and parent for deleting the whole div
+            }else{
+                alert('No More textbox to remove');
+            }
+        });
+
+        $('#buttonget').click(function(){
+            alert($('.textbox').serialize()); // use serialize to get the value of textbox
+        });
+
+        $('#dropdownadd').change(function(){
+            $('#dropdowndiv').html(""); // when the dropdown change set the div to empty
+            $loopcount = $(this).val(); // get the selected value
+            for (var i = 1; i <= $loopcount; i++)
+            {
+                $('#dropdowndiv').append('<div><label>Textbox #'+i+'</label><input type="text" name="textbox2[]" class="textbox2" value="" /></div>');
+            }
+        });
+    });
+</script>
+
+-->
+
+	    @for($i = 1; $i <= 10; $i++)
 	        <div class="form group">
 		        {{ Form::label('q'.$i, 'Question '.$i, array('class' => 'control-label col-sm-2'))}}
 		        <div class="col-sm-10">
@@ -27,11 +67,7 @@ New Evaluation
 	        </div>
 	        <br><br><br>
 	    @endfor
-	    <input type='button' value='Add Question' id='addButton'>
-<input type='button' value='Remove Question' id='removeButton'>
-<br>
 	    <div class="form group">
-	    <br>
 	    	{{ Form::label('close_at', 'Closing Date', array('class' => 'control-label col-sm-2')) }}
 	    	<div class="col-sm-3 col-offset-sm-8">
 	    		{{ Form::text('close_at','',array('class' => 'form-control', 'id' => 'date'))}}
@@ -39,10 +75,6 @@ New Evaluation
 	    	<br><br><br>
 	    <div class="form group">
 	        {{ Form::submit('Create Evaluation', array('class'=>'btn btn-default', 'data-toggle' => 'tooltip','data-placement' => 'top', 'title' => 'Click here to create a new evaluation')) }}
-	   
-
-
-
 	    </div>
 	{{ Form::close() }}
 	<script>$('#date').datepicker();</script>
