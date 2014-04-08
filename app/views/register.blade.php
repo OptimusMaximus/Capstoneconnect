461bb8dc46c1aacf33743845ea6a5341bddf16c5
OptimusMaximus <maximusbrandel@gmail.com>
2014-04-08 18:26:50 -0400

    Added regex check for at least 5 characters for password

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -9,24 +9,30 @@
 
 @section('head')
 <script>
+    //Verify that the two passwords match each other and that they are at least 5 characters long
     $(document).ready(function() {
         $("#submit").click(function(){
             $(".error").hide(); //Clear any previous errors
-            var hasError = false;
+            var validated = false;
             var passwordVal = $("#password").val();
             var checkVal = $("#password-check").val();
+            var validLength = /.{5,}/.test(passwordVal);//Test that password is at least 5 characters long
 
+            if(validLength == false) {
+                $("#password").after('<span class="error">Password must be a length of at least 5 characters.</span>');
+                validated = true;
+            }
             if (passwordVal == '') {
                 $("#password").after('<span class="error">Please enter a password.</span>');
-                hasError = true;
+                validated = true;
             } else if (checkVal == '') {
                 $("#password-check").after('<span class="error">Please re-enter your password.</span>');
-                hasError = true;
+                validated = true;
             } else if (passwordVal != checkVal ) {
                 $("#password-check").after('<span class="error">Passwords do not match.</span>');
-                hasError = true;
+                validated = true;
             }
-            if(hasError == true) {return false;}
+            if(validated == true) {return false;}
         });
     });
 </script>  
