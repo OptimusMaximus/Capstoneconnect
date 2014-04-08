c5286724e8b20ebd2fb08cda8d2ef576c7b53986
OptimusMaximus <maximusbrandel@gmail.com>
2014-04-08 17:59:43 -0400

    Added password reentry for verification

--- app/views/layouts/masterlogin.blade.php
+++ app/views/layouts/masterlogin.blade.php
@@ -21,6 +21,7 @@
         {{ HTML::script('js/jquery-2.0.3.js') }}
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         {{ HTML::script('js/bootstrap.js') }}
+        @yield('head')
     </head>
     <div class="text-center">
         @yield('header')
--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -4,6 +4,34 @@
 @parent
 @stop
 
+@section('styles')
+@stop
+
+@section('head')
+<script>
+    $(document).ready(function() {
+        $("#submit").click(function(){
+            $(".error").hide(); //Clear any previous errors
+            var hasError = false;
+            var passwordVal = $("#password").val();
+            var checkVal = $("#password-check").val();
+
+            if (passwordVal == '') {
+                $("#password").after('<span class="error">Please enter a password.</span>');
+                hasError = true;
+            } else if (checkVal == '') {
+                $("#password-check").after('<span class="error">Please re-enter your password.</span>');
+                hasError = true;
+            } else if (passwordVal != checkVal ) {
+                $("#password-check").after('<span class="error">Passwords do not match.</span>');
+                hasError = true;
+            }
+            if(hasError == true) {return false;}
+        });
+    });
+</script>  
+@stop
+
 @section('header')
 
 @stop
@@ -17,18 +45,25 @@
         <div class='form-group'>
             {{ Form::label('email', 'Email', array('class'=>'col-sm-3 control-label')) }}
             <div class='col-sm-6'>
-                {{ Form::email('email', '', array('class'=>'form-control', 'placeholder' => 'email@sc.edu')) }}
+                {{ Form::email('email', '', array('class'=>'form-control', 'placeholder' => 'username@email.sc.edu')) }}
             </div>
         </div>
 
         <div class='form-group'>
             {{ Form::label('password', 'Password', array('class'=>'col-sm-3 control-label')) }}
             <div class='col-sm-6'>
-                {{ Form::password('password', array('class'=>'form-control')) }}
+                {{ Form::password('password', array('class'=>'form-control', 'id'=>'password')) }}
             </div>
         </div>
 
-        {{ Form::submit('Register', array('class'=>'btn btn-default')) }}
+        <div class='form-group'>
+            {{ Form::label('password', 'Reenter Password', array('class'=>'col-sm-3 control-label')) }}
+            <div class='col-sm-6'>
+                {{ Form::password('password-check', array('class'=>'form-control', 'id'=>'password-check')) }}
+            </div>
+        </div>       
+
+        {{ Form::submit('Register', array('class'=>'btn btn-default', 'id'=>'submit')) }}
     {{ Form::close() }}
 </div>
 @stop
