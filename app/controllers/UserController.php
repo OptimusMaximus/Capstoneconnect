9ceb1c7ccf032eb111d8207791b39eb5ccbded54
OptimusMaximus <maximusbrandel@gmail.com>
2014-09-19 22:16:01 -0400

    Fix for update for laravel

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -144,4 +144,19 @@ class UserController extends \BaseController {
 		return Redirect::to('admin_users');
 	}
 
+	public function getRememberToken()
+	{
+    	return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+    	$this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+    	return 'remember_token';
+	}
+
 }
